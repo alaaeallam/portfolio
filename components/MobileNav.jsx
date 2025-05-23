9886ea87652dda4b4e259059ad3e0d9b13ecc91b
alaaeallam <alaaeallam@gmail.com>
2025-05-23 17:01:36 +0300

    change opens a mobile menu for SEO

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -34,7 +34,10 @@ const MobileNav = () => {
   const pathname = usePathname();
   return (
     <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
+      <SheetTrigger
+  aria-label="Open mobile navigation menu"
+  className="flex justify-center items-center"
+>
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
